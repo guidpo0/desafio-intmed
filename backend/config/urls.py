4abfdaf4db1115ac4518e6439f3e721e5e9f5170
Guilherme Pereira <guilhermepdo1508@gmail.com>
2022-05-23 00:46:39 -0300

    feat: creat url paths

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,21 +1,15 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from medicar import views as medicar_views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'medicos', medicar_views.MedicoViewSet)
+router.register(r'horarios', medicar_views.HorarioViewSet)
+router.register(r'agendas', medicar_views.AgendaViewSet)
+router.register(r'consultas', medicar_views.ConsultaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
--- backend/medicar/views.py
+++ backend/medicar/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from medicar import models as medicar_models
+from medicar import serializers as medicar_serializers
 
-# Create your views here.
+
+class MedicoViewSet(viewsets.ModelViewSet):
+    queryset = medicar_models.Medico.objects.all()
+    serializer_class = medicar_serializers.MedicoSerializer
+
+
+class HorarioViewSet(viewsets.ModelViewSet):
+    queryset = medicar_models.Horario.objects.all()
+    serializer_class = medicar_serializers.HorarioSerializer
+
+
+class AgendaViewSet(viewsets.ModelViewSet):
+    queryset = medicar_models.Agenda.objects.all()
+    serializer_class = medicar_serializers.AgendaSerializer
+
+
+class ConsultaViewSet(viewsets.ModelViewSet):
+    queryset = medicar_models.Consulta.objects.all()
+    serializer_class = medicar_serializers.ConsultaSerializer
