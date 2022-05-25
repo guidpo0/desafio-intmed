c5be23e6a012f6c23d934f3fe66a7733ed319849
Guilherme Pereira <guilhermepdo1508@gmail.com>
2022-05-24 22:47:18 -0300

    feat: change display list in admin

--- backend/medicar/admin.py
+++ backend/medicar/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from medicar import models
 
 
-admin.site.register(models.Medico)
-admin.site.register(models.Agenda)
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'crm')
+    list_display_links = ('id', 'nome')
+
+
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'medico', 'dia')
+    list_display_links = ('id', 'medico')
+
+
+admin.site.register(models.Medico, MedicoAdmin)
+admin.site.register(models.Agenda, AgendaAdmin)
